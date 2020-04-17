346e4d06a6c76e21d2142ab51469427aaf157b83
yujinkim <glimnae@bythem.net>
2020-04-17 11:40:29 +0900

    [init]Set project

---
+++ asset/js/common.js
@@ -0,0 +1,72 @@
+/*************************************************
+## includeHTML
+- include-html 속성을 가진 태그를 찾아, 파일을 가져온다
+*************************************************/
+function includeHTML(callback) {
+  var z, i, elmnt, file, xhr;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    //console.log(file);
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML(callback);
+        }
+      };
+      xhr.open("GET", file, true);
+      xhr.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+  setTimeout(function() {
+    callback();
+  }, 0);
+}
+/*************************************************
+## includeRouter
+- route-link 속성을 가진 GNB를 클릭할 경우, #content 태그에 해당 파일을 불러온다
+*************************************************/
+function includeRouter(cb) {
+  var content, file, xhttp, i;
+  document.body.addEventListener("click", e => {
+    file = e.target.getAttribute("route-link");
+    if (file) {
+      content = document.getElementById("content");
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            content.innerHTML = this.responseText;
+            var scripts = content.getElementsByTagName("script");
+            for (var i = 0; i < scripts.length; i++) {
+              eval(scripts[i].text);
+            }
+            setTimeout(function() {
+              cb(e);
+            }, 0);
+          }
+          if (this.status == 404) {
+            content.innerHTML = "Page not found.";
+          }
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+    }
+  });
+}
